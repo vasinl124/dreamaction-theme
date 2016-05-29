0baeb0d76301629e873207649382f6f2f8794284
Vasin Max Limsukhawat <max.limsukhawat@qsc.com>
2016-05-29 00:43:27 -0600

    asdf

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,5 @@
 $(document).ready(function(){
   $('.owl-carousel').owlCarousel({
-    stagePadding: 100,
     items:1,
     loop:true,
     margin:10,
@@ -9,5 +8,23 @@ $(document).ready(function(){
     autoplayHoverPause:true,
     lazyLoad: true,
     // nav:true
+    responsive : {
+        // breakpoint from 0 up
+        0 : {
+            stagePadding: 0,
+        },
+        // breakpoint from 480 up
+        480 : {
+            stagePadding: 0,
+        },
+        // breakpoint from 768 up
+        768 : {
+            stagePadding: 50,
+        },
+        // breakpoint from 1200 up
+        1200 : {
+            stagePadding: 100,
+        }
+    }
   });
 });
